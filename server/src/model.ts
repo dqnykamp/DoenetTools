15036daf4baad596fd139a4b38b69700e73fd90a
Duane Nykamp <nykamp@umn.edu>
2025-01-22 11:44:56 -0600

    merge new prefill

--- server/src/model.ts
+++ server/src/model.ts
@@ -115,6 +115,9 @@ export async function createActivity(
     }
   }
 
+  const sourcePrefill = `<p>Your content goes here! Example: What is <m>1+1</m>?</p>
+<answer>2</answer>`;
+
   const activity = await prisma.content.create({
     data: {
       ownerId,
@@ -128,7 +131,7 @@ export async function createActivity(
       documents: {
         create: [
           {
-            source: "",
+            source: sourcePrefill,
             doenetmlVersionId: defaultDoenetmlVersion.id,
             name: "Untitled Document",
           },
